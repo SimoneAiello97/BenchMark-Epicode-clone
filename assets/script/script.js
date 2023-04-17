cc17f864a94ba25cc340090ba328f5988dc6bf57
arturocfr <arturofabian504@gmail.com>
2023-04-17 23:30:56 +0200

    1 giorno

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+function verificaCheckbox() {
+    let casella = document.getElementById('miaCheckbox');
+    if (!casella.checked) {
+        alert('Per favore, seleziona la casella di controllo.');
+    }
+}
