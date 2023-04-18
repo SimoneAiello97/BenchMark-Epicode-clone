18e0f3a845e02ea2d7337ce3288c77a2a860b9b5
arturocfr <arturofabian504@gmail.com>
2023-04-18 14:44:40 +0200

    rating-star

---
+++ asset/script/script.js
@@ -0,0 +1,11 @@
+document.getElementById('rating-star').innerHTML=
+`<div class="centro">
+<div>
+    <p class="title">Tell us how it's going</p>
+</div>
+    <div>
+    <p>From 0 to 10,how likely are you to recommend Epicode<br>
+    to a friend or acolleague?
+    </p>
+
+</div>`
