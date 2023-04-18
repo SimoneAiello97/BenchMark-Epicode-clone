01da85357c0756d26ec6f2486ab3e730cc75df96
arturocfr <arturofabian504@gmail.com>
2023-04-18 17:34:33 +0200

    fine 2° giornata

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,47 +1,34 @@
 document.getElementById('rating-star').innerHTML=
 `<div class="centro">
-<div>
-    <p class="title-star">Tell us how it's going</p>
-</div>
     <div>
-    <p class="subtitle-star">From 0 to 10,how likely are you to recommend Epicode<br>
-    to a friend or acolleague?
-    </p>
-    <div class="star-rating">
-        <i class="fa fa-star" data-index="0"></i>
-        <i class="fa fa-star" data-index="1"></i>
-        <i class="fa fa-star" data-index="2"></i>
-        <i class="fa fa-star" data-index="3"></i>
-        <i class="fa fa-star" data-index="4"></i>
-        <i class="fa fa-star" data-index="5"></i>
-        <i class="fa fa-star" data-index="6"></i>
-        <i class="fa fa-star" data-index="7"></i>
-        <i class="fa fa-star" data-index="8"></i>
-        <i class="fa fa-star" data-index="9"></i>
+        <p class="title-star">Tell us how it's going</p>
     </div>
-
+    <div>
+        <p class="subtitle-star">From 0 to 10,how likely are you to recommend Epicode<br>
+        to a friend or acolleague?
+        </p>
+    </div>
+    <div class="star-rating">
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+        <div class="fastar"> </div>
+    </div>
+    <div>
+    <p class="subtitle-star">Leave us an open feedback about your experience so far
+    </p>
+    </div>
+    <div class= cont-text>
+    <input class="textarea" type="text" placeholder="Write your comment here">
+    </div>
+    </div>
+    <div class="cont-bottone2">
+        <button class="bottone2" onclick="verificaCheckbox()">PROCEED</button>
+    </div>
 </div>`
-
-const stars = document.querySelectorAll(".star-rating i");
-
-function setRating(index) {
-  stars.forEach((star, i) => {
-    if (i <= index) {
-      star.classList.add("active");
-    } else {
-      star.classList.remove("active");
-    }
-  });
-}
-
-stars.forEach((star, index) => {
-  star.addEventListener("click", () => {
-    setRating(index);
-  });
-  star.addEventListener("mouseover", () => {
-    setRating(index);
-  });
-  star.addEventListener("mouseout", () => {
-    setRating(-1);
-  });
-});
