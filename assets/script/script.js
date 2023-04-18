0e65187f8ba3eff19a4a6958c8161fb4951e857a
arturocfr <arturofabian504@gmail.com>
2023-04-18 09:20:07 +0200

    fine

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,6 @@
 function verificaCheckbox() {
     let casella = document.getElementById('miaCheckbox');
     if (!casella.checked) {
-        alert('Per favore, seleziona la casella di controllo.');
+        alert('Per favore, seleziona la casella per continuare');
     }
 }
